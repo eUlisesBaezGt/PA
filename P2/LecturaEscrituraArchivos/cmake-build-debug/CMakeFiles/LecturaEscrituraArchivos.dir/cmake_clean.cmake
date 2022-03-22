e600e12e5c11c751afa8b0b146ee02373b58328a
eUlisesBaezGt <0241823@up.edu.mx>
2022-03-22 14:05:14 -0600

    Creation

---
+++ P2/LecturaEscrituraArchivos/cmake-build-debug/CMakeFiles/LecturaEscrituraArchivos.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LecturaEscrituraArchivos.dir/main.cpp.o"
+  "CMakeFiles/LecturaEscrituraArchivos.dir/main.cpp.o.d"
+  "LecturaEscrituraArchivos"
+  "LecturaEscrituraArchivos.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LecturaEscrituraArchivos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
